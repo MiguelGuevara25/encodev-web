9deda31332d1eaae2a7d471c2de543892d350d37
Encode-dev <encode_dev@hotmail.com>
2026-06-23 10:34:06 -0500

    config for deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // <-- Añade esta línea
+  // Si usas imágenes con el componente <Image /> de Next, añade esto también:
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
